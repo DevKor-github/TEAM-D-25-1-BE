d2cb9288c481cbb26a51f1a18ca50e3d81b8a071
Juwoong Bae <juungbae@gmail.com>
2025-05-29 18:55:16 +0900

    feat: acceptFollow

---
+++ src/user/usecases/acceptFollow.ts
@@ -0,0 +1,24 @@
+import { FollowerStatus } from '@prisma/client';
+import { FollowerRepository } from '../repositories/follower';
+import { NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AcceptFollowUseCase {
+  constructor(private readonly followerRepository: FollowerRepository) {}
+
+  async execute(userId: string, followerId: string) {
+    const follower = await this.followerRepository.getFollower({
+      userId,
+      followerId,
+    });
+
+    if (!follower || follower.status !== FollowerStatus.PENDING) {
+      throw new NotFoundException('No pending follower found');
+    }
+
+    return this.followerRepository.updateFollowerStatus(userId, followerId, {
+      status: FollowerStatus.ACCEPTED,
+    });
+  }
+}
