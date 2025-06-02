438f024012b535b1b3fa67b733ff5a0b142a43e9
Na1gae <nalgae@korea.ac.kr>
2025-06-02 14:49:45 +0900

    mod: 온보딩 과정 강제

--- src/auth/guards/firebase-auth.guard.ts
+++ src/auth/guards/firebase-auth.guard.ts
@@ -1,7 +1,8 @@
 import { 
   Injectable, 
   ExecutionContext, 
-  UnauthorizedException 
+  UnauthorizedException,
+  ForbiddenException
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -10,6 +11,9 @@ export class FirebaseAuthGuard extends AuthGuard('firebase-auth') {
   handleRequest(err: any, user: any, info: Error, context: ExecutionContext) {
     if (err || !user) {
       throw err || new UnauthorizedException('유효하지 않은 Firebase 토큰입니다.');
+    }
+    if (!user.isOnboarded) {
+      throw new ForbiddenException('온보딩을 완료해야 서비스를 이용할 수 있습니다.');
     }
     return user;
   }
