d079b75c22d60a44f6737b45b80d97f2b21f3efe
Na1gae <nalgae@korea.ac.kr>
2025-06-02 14:48:20 +0900

    chore: adminCredential 가져오기 방식 변경

--- src/auth/module.ts
+++ src/auth/module.ts
@@ -3,43 +3,43 @@ import { PassportModule } from "@nestjs/passport";
 import * as admin from "firebase-admin"
 import { FirebaseAuthStrategy } from "./strategies/firebase.strategy";
 import { GoogleStrategy } from "./strategies/google.strategy";
-import { AppleStrategy } from "./strategies/apple.strategy";
-import { APP_GUARD } from "@nestjs/core";
+import { AppleStrategy } from "./strategies/apple.strategy"; 
 import { FirebaseAuthGuard } from "./guards/firebase-auth.guard";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthController } from './controller';
+import { AuthService } from "./service";
+import { PrismaModule } from "../prisma/prisma.module";
+
+const serviceAccount = require('../../firebase-adminsdk.json');
 
 @Module({
     imports: [
         ConfigModule,
-        PassportModule.register({ defaultStrategy: 'firebase-auth' })    
+        PassportModule.register({ defaultStrategy: 'firebase-auth' }),
+        PrismaModule
     ],
     providers: [
+        AuthService,
         {
             provide: 'FIREBASE_ADMIN',
             useFactory: (config: ConfigService) => {
-                if (!admin.apps.length){
+                const APP_NAME = 'GrooApp';
+                if (!admin.apps.length || !admin.apps.find(app => app.name === APP_NAME)){
                     return admin.initializeApp({
-                        credential: admin.credential.cert({
-                            projectId: config.get<string>("FIREBASE_PROJECT_ID"),
-                            clientEmail: config.get<string>("FIREBASE_CLIENT_EMAIL"),
-                            privateKey: decodeURIComponent( //PrivateKey는 URL Encode 필요
-                                config.get<string>("FIREBASE_PRIVATE_KEY")
-                            )
-                        })
-                    })
+                        credential: admin.credential.cert(
+                            serviceAccount as admin.ServiceAccount
+                        ),
+                        projectId: 'groo-test'
+                    }, APP_NAME)
                 }
-                return admin.app()
+                return admin.app(APP_NAME)
             },
             inject: [ConfigService]
         },
         FirebaseAuthStrategy,
         GoogleStrategy,
         AppleStrategy,
-        {
-            provide: APP_GUARD,
-            useClass: FirebaseAuthGuard
-        }
+        FirebaseAuthGuard,
     ],
     exports: [FirebaseAuthGuard],
     controllers: [AuthController]
