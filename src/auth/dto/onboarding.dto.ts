df0c1bd0f20987f3b03902f304f578d531729f8a
Na1gae <nalgae@korea.ac.kr>
2025-06-05 17:25:28 +0900

    feat: 온보딩 정보에 인스타그램 형식의 username 추가

--- src/auth/dto/onboarding.dto.ts
+++ src/auth/dto/onboarding.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsString, Length, Matches } from "class-validator";
 
 export class OnboardingInfoDto{
-    @IsNotEmpty()
     @IsString()
-    nickname: string;
-
-    // TODO: 온보딩에 필요한 다른 필드 추가 및 데코레이터 적용
+    @Length(1, 30, { message: '사용자명은 1자 이상 30자 이하여야 합니다.' })
+    @Matches(/^(?!.*\.\.)(?!.*\.$)[a-zA-Z0-9._]+$/, {
+      message: '사용자명은 영문자, 숫자, 밑줄(_) 및 마침표(.)만 사용할 수 있으며, 마침표로 끝나거나 연속해서 사용할 수 없습니다.',
+    })
+    username: string;
+    // TODO: 온보딩에 필요한 다른 필드 추가
 }
