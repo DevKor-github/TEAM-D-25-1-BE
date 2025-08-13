f5af762fd6736fc83892103681f031642961e7e2
Juwoong Bae <juungbae@gmail.com>
2025-08-13 19:30:12 +0900

    hotfix: remove images on code

--- src/images/service.ts
+++ src/images/service.ts
@@ -6,30 +6,30 @@ import { PrismaService } from '@/prisma/prisma.service';
 export class ImagesService {
   constructor(
     private readonly s3Service: S3Service,
-    private readonly prisma: PrismaService
+    private readonly prisma: PrismaService,
   ) {}
 
   async uploadSeedImage(file: Express.Multer.File) {
     const result = await this.s3Service.uploadImage(file, 'images/seed');
-    await this.prisma.images.create({ data: { key: result.key } });
+    // await this.prisma.images.create({ data: { key: result.key } });
     return { url: result.url };
   }
 
   async uploadUserProfileImage(file: Express.Multer.File) {
     const result = await this.s3Service.uploadImage(file, 'images/profile');
-    await this.prisma.images.create({ data: { key: result.key } });
+    // await this.prisma.images.create({ data: { key: result.key } });
     return { url: result.url };
   }
 
   async uploadRestaurantImage(file: Express.Multer.File) {
     const result = await this.s3Service.uploadImage(file, 'images/restaurant');
-    await this.prisma.images.create({ data: { key: result.key } });
+    // await this.prisma.images.create({ data: { key: result.key } });
     return { url: result.url };
   }
 
   async uploadReviewImage(file: Express.Multer.File) {
     const result = await this.s3Service.uploadImage(file, 'images/review');
-    await this.prisma.images.create({ data: { key: result.key } });
+    // await this.prisma.images.create({ data: { key: result.key } });
     return { url: result.url };
   }
 
@@ -37,11 +37,11 @@ export class ImagesService {
     await this.s3Service.deleteImage(key);
 
     await this.prisma.$transaction([
-      this.prisma.images.delete({ where: { key } }),
+      // this.prisma.images.delete({ where: { key } }),
 
       this.prisma.user.updateMany({
         where: { profileImageUrl: key },
-        data: { profileImageUrl: null }
+        data: { profileImageUrl: null },
       }),
 
       this.prisma.$executeRaw`
@@ -55,6 +55,6 @@ export class ImagesService {
         SET images = array_remove(images, ${key})
         WHERE ${key} = ANY(images)
       `,
-    ])
+    ]);
   }
 }
