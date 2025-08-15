7fcf4e3a41fb61094c6d4160f59719e0d115d39a
Na1gae <nalgae@korea.ac.kr>
2025-08-15 19:26:27 +0900

    feat: 나무 관련 정보를 Settings에서 불러오도록 수정

--- src/tree/constants.ts
+++ src/tree/constants.ts
@@ -1,3 +1,4 @@
+import { TreeType } from "@/settings/dto";
 import { Tag } from "@prisma/client";
 
 export const TAG_KOREAN_MAP: { [key in Tag]: string } = {
@@ -91,3 +92,24 @@ export const STYLE_TAG_KOREAN_MAP: Partial<{ [key in Tag]: string }> = Object.fr
 export const FOOD_TAG_KOREAN_MAP: Partial<{ [key in Tag]: string }> = Object.fromEntries(
   FOOD_TAGS.map(tag => [tag, TAG_KOREAN_MAP[tag]])
 );
+
+export const TREE_TYPES: TreeType[] = [
+  {
+    key: 0,
+    name: '참나무',
+    levels: [
+      { level: 1, imageUrl: 'https://groo-static.s3.ap-northeast-2.amazonaws.com/images/tree/chamnamu_1.png' },
+      { level: 2, imageUrl: 'https://groo-static.s3.ap-northeast-2.amazonaws.com/images/tree/chamnamu_2.png' },
+      { level: 3, imageUrl: 'https://groo-static.s3.ap-northeast-2.amazonaws.com/images/tree/chamnamu_3.png' },
+    ],
+  },
+  {
+    key: 1,
+    name: '침엽수',
+    levels: [
+      { level: 1, imageUrl: 'https://groo-static.s3.ap-northeast-2.amazonaws.com/images/tree/chimyeopsu_1.png' },
+      { level: 2, imageUrl: 'https://groo-static.s3.ap-northeast-2.amazonaws.com/images/tree/chimyeopsu_1.png' },
+      { level: 3, imageUrl: 'https://groo-static.s3.ap-northeast-2.amazonaws.com/images/tree/chimyeopsu_1.png' },
+    ],
+  },
+];
