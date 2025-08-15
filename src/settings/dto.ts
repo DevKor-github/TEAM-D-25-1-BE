d4cb40585b82813527a7a7ff0d53906841266c5f
Na1gae <nalgae@korea.ac.kr>
2025-08-15 15:14:49 +0900

    feat: Settings에 Mbti, Tag 추가

--- src/settings/dto.ts
+++ src/settings/dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Mbti } from '@prisma/client';
 
-export interface FoodTag {
+export interface Tag {
   key: string;
   value: string;
 }
@@ -8,7 +9,10 @@ export interface FoodTag {
 export interface AppSettings {
   maintenance: boolean;
   minimalVersion: string;
-  foodTags: FoodTag[];
+  foodTags: Tag[];
+  styleTags: Tag[];
+  tags: Tag[];
+  mbti: Mbti[];
 }
 
 export class GetSettingsResponse {
@@ -59,6 +63,25 @@ export class UpdateSettingsRequest {
     items: {
       type: 'object',
       properties: {
+        key: { type: 'string', example: 'STEAK' },
+        value: { type: 'string', example: '스테이크' },
+      },
+    },
+    example: [
+      { key: 'STEAK', value: '스테이크' },
+      { key: 'PASTA', value: '파스타' },
+      { key: 'PIZZA', value: '피자' },
+    ],
+    required: false,
+  })
+  foodTags?: Tag[];
+
+  @ApiProperty({
+    description: '스타일 태그 목록',
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
         key: { type: 'string', example: 'DRINKER' },
         value: { type: 'string', example: '애주가' },
       },
@@ -69,8 +92,44 @@ export class UpdateSettingsRequest {
       { key: 'SPICY_FOOD_LOVER', value: '맵부심' },
     ],
     required: false,
+  })  
+  styleTags?: Tag[];
+
+  @ApiProperty({
+    description: '전체 태그 목록 (음식 + 스타일)',
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        key: { type: 'string', example: 'DRINKER' },
+        value: { type: 'string', example: '애주가' },
+      },
+    },
+    example: [
+        { key: 'DRINKER', value: '애주가' },
+        { key: 'STEAK', value: '스테이크' },
+    ],
+    required: false,
+  })  
+  tags?: Tag[];
+
+  @ApiProperty({
+    description: 'MBTI Enum 목록',
+    type: 'array',
+    items: {
+      type: 'string',
+      enum: [
+        'ESTJ', 'INFP', 'ISFP', 'INTJ', 'ENTJ', 'ISFJ', 'ESFP', 'ISTP',
+        'INFJ', 'ENTP', 'ISTJ', 'ESTP', 'ENFJ', 'ESFJ', 'ENFP', 'INTP'
+      ],
+    },
+    example: [
+        'ESTJ', 'INFP', 'ISFP', 'INTJ', 'ENTJ', 'ISFJ', 'ESFP', 'ISTP',
+        'INFJ', 'ENTP', 'ISTJ', 'ESTP', 'ENFJ', 'ESFJ', 'ENFP', 'INTP'
+      ],
+    required: false,
   })
-  foodTags?: FoodTag[];
+  mbti?: Mbti[];
 }
 
 export class UpdateSettingsResponse {
--- src/settings/service.ts
+++ src/settings/service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { FOOD_TAG_KOREAN_MAP } from '@/tree/constants';
-import { AppSettings, FoodTag } from './dto';
+import { 
+  FOOD_TAG_KOREAN_MAP, 
+  STYLE_TAG_KOREAN_MAP, 
+  TAG_KOREAN_MAP 
+} from '@/tree/constants';
+import { AppSettings, Tag } from './dto';
+import { Mbti } from '@prisma/client';
 
 @Injectable()
 export class SettingsService {
+  
+  private convertMapToArray(param: Record<string, string>): Tag[] {
+    return Object.entries(param).map(([key, value]) => ({ key, value }));
+  }
+
   async getSettings(): Promise<AppSettings> {
     // TODO: Implement get app settings logic
-    const foodTags: FoodTag[] = Object.entries(FOOD_TAG_KOREAN_MAP).map(
-      ([key, value]) => ({
-        key,
-        value,
-      }),
-    );
+    
+    const tags = this.convertMapToArray(TAG_KOREAN_MAP)
+    const foodTags = this.convertMapToArray(FOOD_TAG_KOREAN_MAP)
+    const styleTags = this.convertMapToArray(STYLE_TAG_KOREAN_MAP)
+    const mbti = Object.values(Mbti);
 
     return {
       maintenance: false,
       minimalVersion: '1.0.0',
+      tags,
       foodTags,
+      styleTags,
+      mbti
     };
   }
 
